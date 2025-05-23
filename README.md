7e2cdbb7d2f214e7b27a53a6c8fb239fdd943774
Ashish Dwivedi <ashish.dwivedi@Ashish.local>
2025-05-23 12:16:31 +0530

    new backend fastapi server

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# selfHelp-server
+After creating venv using python3.10 run
+ 
+python3.10 seed_db.py  
+
+
+intstall requirements.txt then run
+
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
--- app/schemas/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.schemas.user import *
-from app.schemas.ai_models import *
-from app.schemas.conversation import *
-from app.schemas.content import *
-from app.schemas.integration import *
-from app.schemas.utility import *
